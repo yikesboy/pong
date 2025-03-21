bc4a4d166fde9d1bb9f727c0cb34fc68d8c3779f
JL <115737916+yikesboy@users.noreply.github.com>
2025-03-21 21:43:19 +0100

    fix: resolved ball hitbox edgecases

--- update.lua
+++ update.lua
@@ -19,25 +19,25 @@ end
 
 function validate_ball_position()
     if game.ball.pos_x <= (game.paddles.left.pos_x + game.paddles.left.width)
-        and game.ball.pos_y >= game.paddles.left.pos_y
+        and game.ball.pos_y >= game.paddles.left.pos_y - game.ball.radius
         and game.ball.pos_y <= (game.paddles.left.pos_y + game.paddles.left.height)
     then
        game.ball.velocity.x = math.abs(game.ball.velocity.x)
     end
 
     if game.ball.pos_x >= game.paddles.right.pos_x
-        and game.ball.pos_y >= game.paddles.right.pos_y
+        and game.ball.pos_y >= game.paddles.right.pos_y - game.ball.radius
         and game.ball.pos_y <= game.paddles.right.pos_y + game.paddles.right.height
     then
        game.ball.velocity.x = -math.abs(game.ball.velocity.x)
     end
 
-    if game.ball.pos_x >= game.window.width then
+    if game.ball.pos_x >= game.window.width + game.ball.radius then
         spawn_ball()
         game.players.left.score = game.players.left.score + 1
     end
 
-    if game.ball.pos_x <= 0 then
+    if game.ball.pos_x <= 0 - game.ball.radius * 2 then
         spawn_ball()
         game.players.right.score = game.players.right.score + 1
     end
