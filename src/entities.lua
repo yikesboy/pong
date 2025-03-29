b0a8f33e8d0e33f1b1ee441a38a74ef2c65db227
JL <115737916+yikesboy@users.noreply.github.com>
2025-03-29 21:28:40 +0100

    feat: added type annotations and various fields to entities

--- src/entities.lua
+++ src/entities.lua
@@ -1,5 +1,24 @@
 local config = require("config")
 
+---@class Ball
+---@field x number
+---@field y number
+---@field radius number
+---@field speed { x: number, y: number }
+
+---@class Paddle
+---@field x number
+---@field y number
+---@field height number
+---@field width number
+---@field speed number
+---@field score number
+
+---@class Entities
+---@field ball Ball
+---@field left_paddle Paddle
+---@field right_paddle Paddle
+
 local entities = {
     ball = {
         x = 0,
@@ -11,13 +30,17 @@ local entities = {
         x = 0,
         y = 0,
         height = config.PADDLE_HEIGHT,
-        width = config.PADDLE_WIDTH
+        width = config.PADDLE_WIDTH,
+        speed = config.PADDLE_SPEED,
+        score = 0
     },
     right_paddle = {
         x = 0,
         y = 0,
         height = config.PADDLE_HEIGHT,
-        width = config.PADDLE_WIDTH
+        width = config.PADDLE_WIDTH,
+        speed = config.PADDLE_SPEED,
+        score = 0
     },
 }
 
