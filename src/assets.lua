020dd4564865261d4faea658b2d7ec2fc2cbcab2
JL <115737916+yikesboy@users.noreply.github.com>
2025-03-29 22:28:19 +0100

    feat: implemented asset store/loader

---
+++ src/assets.lua
@@ -0,0 +1,14 @@
+local config = require("config")
+
+local assets = {
+    fonts = {},
+    sounds = {}
+}
+
+function assets.load()
+    assets.sounds.background_music = love.audio.newSource("assets/sounds/661104__evapofficial__synth-bass-loop-28.wav", "static")
+    assets.sounds.hit_sfx = love.audio.newSource("assets/sounds/4359__noisecollector__pongblipf4.wav", "static")
+    assets.fonts.main = love.graphics.newFont("assets/fonts/Arcadepix Plus.ttf")
+end
+
+return assets
