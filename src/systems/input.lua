c4a540d579632236539af2bac6aa2e32e318ca0b
JL <115737916+yikesboy@users.noreply.github.com>
2025-03-29 22:53:51 +0100

    fix: added missing deltatime as function parameter

--- src/systems/input.lua
+++ src/systems/input.lua
@@ -1,6 +1,9 @@
 local input = {}
 
-function input.update(paddle)
+---Processes user input for each frame.
+---@param dt number The time elapsed since the last frame (in seconds).
+---@param paddle Paddle
+function input.update(dt, paddle)
     if love.keyboard.isDown("up") then
         paddle.y = paddle.y - paddle.speed * dt
     elseif love.keyboard.isDown("down") then
