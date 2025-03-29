f98af3832558dd930ae73ce6985a323c66646f85
JL <115737916+yikesboy@users.noreply.github.com>
2025-03-29 22:30:39 +0100

    feat: implemented util function to normalize text cords

---
+++ src/utils.lua
@@ -0,0 +1,17 @@
+local assets = require("src.assets")
+
+local utils = {}
+
+--- Calculates normalized coordinates such that the text is centered at the given position.
+--- @param text string The text whose dimensions are used for normalization.
+--- @param x number The original x-coordinate.
+--- @param y number The original y-coordinate.
+--- @return number x_norm The normalized x-coordinate.
+--- @return number y_norm The normalized y-coordinate.
+function utils.get_normalized_coordinates(text, x, y)
+    local x_norm = x - game.window.font:getWidth(text) * 0.5
+    local y_norm = y - game.window.font:getHeight() * 0.5
+    return x_norm, y_norm
+end
+
+return utils
