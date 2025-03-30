e02a0da26028d749800986d09cdf7e547fe38566
JL <115737916+yikesboy@users.noreply.github.com>
2025-03-30 17:55:19 +0200

    feat: created a large main title font

--- src/assets.lua
+++ src/assets.lua
@@ -9,6 +9,7 @@ function assets.load()
     assets.sounds.background_music = love.audio.newSource("assets/sounds/661104__evapofficial__synth-bass-loop-28.wav", "static")
     assets.sounds.hit_sfx = love.audio.newSource("assets/sounds/4359__noisecollector__pongblipf4.wav", "static")
     assets.fonts.main_large = love.graphics.newFont("assets/fonts/Arcadepix Plus.ttf", config.WINDOW_HEIGHT / 10)
+    assets.fonts.title = love.graphics.newFont("assets/fonts/Arcadepix Plus.ttf", config.WINDOW_HEIGHT / 4)
 end
 
 return assets
