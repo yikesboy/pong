9e5c78c44c35b54cb0e8e98d3025354bba522f3b
JL <115737916+yikesboy@users.noreply.github.com>
2025-04-02 21:15:41 +0200

    feat: removed background music

--- src/assets.lua
+++ src/assets.lua
@@ -6,7 +6,6 @@ local assets = {
 }
 
 function assets.load()
-    assets.sounds.background_music = love.audio.newSource("assets/sounds/661104__evapofficial__synth-bass-loop-28.wav", "static")
     assets.sounds.hit_sfx = love.audio.newSource("assets/sounds/4359__noisecollector__pongblipf4.wav", "static")
     assets.sounds.round_loss = love.audio.newSource("assets/sounds/572937__bloodpixelhero__error-2.wav", "static")
     assets.sounds.switch_sfx = love.audio.newSource("assets/sounds/457326__stratocube__switch-05.wav", "static")
